9f61309817ae486e2e66df93348a2a5c35f847eb
MariusB <bira_marius_gabriel@yahoo.com>
2024-04-28 22:11:51 +0300

    Controllers added before testing

--- src/main/java/com/example/shoppingcart/entity/Cart.java
+++ src/main/java/com/example/shoppingcart/entity/Cart.java
@@ -1,8 +1,25 @@
 package com.example.shoppingcart.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
 
 @Entity
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long cartId;
+
+    @Column
+    //add foreign key
+    private long clientId;
+
+    @Column
+    //add foreign key
+    private long orderId;
+
+    @Column
+    private BigDecimal total;
+
+    //add for cart item amount and price (calculating price by amount)
 }
