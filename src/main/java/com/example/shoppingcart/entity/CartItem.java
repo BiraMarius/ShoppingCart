0fddb5375945529f9b1da0ad9ce5003ad2c83a80
MariusB <bira_marius_gabriel@yahoo.com>
2024-05-03 22:02:44 +0300

    cartItem table done

--- src/main/java/com/example/shoppingcart/entity/CartItem.java
+++ src/main/java/com/example/shoppingcart/entity/CartItem.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 
 @Entity(name="CartItem")
 @Table(name="cart_item")
+@Data
 public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,14 +17,14 @@ public class CartItem {
     @JoinColumn(name = "Product_id")
     private Product product;
 
-//    @Column
-//    private String name;
-//
-//    @Column
-//    private String brand;
-//
-//    @Column
-//    private String smallDescription;
+    @Column
+    private String name;
+
+    @Column
+    private String brand;
+
+    @Column
+    private String smallDescription;
 
     @Column
     private int amount;
